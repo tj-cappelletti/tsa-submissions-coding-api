f430df7520472c9fa137a60e011fa6f2e58106fc
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2022-02-13 14:59:51 -0500

    Setup API to authenticate JWT tokens
    Added logic to work in Docker Compose
    Need to trust root CA for ASP.NET certs for calls to work

--- src/Tsa.Submissions.Coding.WebApi/Startup.cs
+++ src/Tsa.Submissions.Coding.WebApi/Startup.cs
@@ -1,59 +1,86 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
-namespace Tsa.Submissions.Coding.WebApi
+namespace Tsa.Submissions.Coding.WebApi;
+
+public class Startup
 {
-    public class Startup
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            Configuration = configuration;
-        }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tsa.Submissions.Coding.WebApi v1"));
 
-        public IConfiguration Configuration { get; }
+            // In a development setting, all containers use the ASP.NET development certificate
+            // Since this is a self signed certificate, the root CA must be trusted
+            // The Docker Compose mounts a volume in the container for the root CA certificate
+            // This code block undates the trusted root CA so ASP.NET development certificates work
+            var dockerContainer = Configuration["DOCKER_CONTAINER"] != null && Configuration["DOCKER_CONTAINER"] == "Y";
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
+            if (dockerContainer)
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Tsa.Submissions.Coding.WebApi", Version = "v1" });
-            });
-        }
+                var updateCaCertificatesProcess = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        Arguments = "update-ca-certificates",
+                        CreateNoWindow = true,
+                        FileName = "/bin/bash",
+                        UseShellExecute = false
+                    }
+                };
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tsa.Submissions.Coding.WebApi v1"));
+                updateCaCertificatesProcess.Start();
+                updateCaCertificatesProcess.WaitForExit();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var requireAuthenticatedUserPolicy = new AuthorizationPolicyBuilder()
+            .RequireAuthenticatedUser()
+            .Build();
+
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+            {
+                options.Authority = Configuration["Authentication:Authority"];
+
+                options.TokenValidationParameters = new TokenValidationParameters { ValidateAudience = false };
+            });
+
+        services.AddControllers(configure => { configure.Filters.Add(new AuthorizeFilter(requireAuthenticatedUserPolicy)); });
+
+        services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Tsa.Submissions.Coding.WebApi", Version = "v1" }); });
     }
 }
