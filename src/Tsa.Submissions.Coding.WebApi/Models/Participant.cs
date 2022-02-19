e9ebcad5231443cc006d4cf92b0494421c741dbc
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2022-02-19 11:10:28 -0500

    Removed unused property
    Not needed since this is a child object of the team
    Applied ReSharper formatting

--- src/Tsa.Submissions.Coding.WebApi/Models/Participant.cs
+++ src/Tsa.Submissions.Coding.WebApi/Models/Participant.cs
@@ -1,17 +1,10 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-
-namespace Tsa.Submissions.Coding.WebApi.Models;
+﻿namespace Tsa.Submissions.Coding.WebApi.Models;
 
 public class Participant
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string ParticipantId => $"{SchoolNumber}-{ParticipantNumber}";
 
     public string ParticipantNumber { get; set; }
 
     public string SchoolNumber { get; set; }
-
-    public string ParticipantId => $"{SchoolNumber}-{ParticipantNumber}";
 }
