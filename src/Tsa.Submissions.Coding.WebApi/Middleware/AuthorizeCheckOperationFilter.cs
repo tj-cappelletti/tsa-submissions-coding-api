4d2e186a5c7940e0feab8774afa1d33742457240
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2022-02-19 11:11:16 -0500

    Added operation filter for Swagger
    This allows authentication to work for all API calls

---
+++ src/Tsa.Submissions.Coding.WebApi/Middleware/AuthorizeCheckOperationFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Tsa.Submissions.Coding.WebApi.Middleware;
+
+public class AuthorizeCheckOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
+        operation.Responses.Add("403", new OpenApiResponse { Description = "Forbidden" });
+
+        operation.Security = new List<OpenApiSecurityRequirement>
+        {
+            new()
+            {
+                [
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "oauth2"
+                        }
+                    }
+                ] = new[] { "tsa.coding.submissions.read", "tsa.coding.submissions.create" }
+            }
+        };
+    }
+}
