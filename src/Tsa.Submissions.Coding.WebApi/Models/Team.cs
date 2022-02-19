f5582be46590397f775804aa9e6435f37d8ad80c
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2022-02-19 11:11:47 -0500

    Added data annotations

--- src/Tsa.Submissions.Coding.WebApi/Models/Team.cs
+++ src/Tsa.Submissions.Coding.WebApi/Models/Team.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Tsa.Submissions.Coding.WebApi.Models;
 
@@ -8,6 +9,7 @@ public class Team
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
+    [SwaggerSchema(ReadOnly = true)]
     public string Id { get; set; }
 
     public string SchoolNumber { get; set; }
