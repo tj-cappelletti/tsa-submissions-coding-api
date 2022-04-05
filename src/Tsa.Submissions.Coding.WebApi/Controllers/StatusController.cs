e164bca792db42ab7aac5acf96edc01e7e644537
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2022-04-05 09:20:01 -0400

    Added Status Endpoint (#8)
    * Added status check to Teams service
    
    * Added controller to return status of API
    
    As services get added, additional checks will be added
    
    * Added unit tests for StatusController

---
+++ src/Tsa.Submissions.Coding.WebApi/Controllers/StatusController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tsa.Submissions.Coding.WebApi.Models;
+using Tsa.Submissions.Coding.WebApi.Services;
+
+namespace Tsa.Submissions.Coding.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StatusController : ControllerBase
+{
+    private readonly ITeamsService _teamsService;
+
+    public StatusController(ITeamsService teamsService)
+    {
+        _teamsService = teamsService;
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var status = new Status
+        {
+            TeamsServiceIsAlive = await _teamsService.Ping()
+        };
+
+        return status.IsHealthy
+            ? Ok(status)
+            : StatusCode(StatusCodes.Status500InternalServerError, status);
+    }
+}
