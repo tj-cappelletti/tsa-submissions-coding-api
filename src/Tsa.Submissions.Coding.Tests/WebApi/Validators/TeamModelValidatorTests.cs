8f3d525a5898000ee18786b6379f2d2f29a70404
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2022-12-10 11:45:37 -0500

    Created unit test for validating TeamModel

---
+++ src/Tsa.Submissions.Coding.Tests/WebApi/Validators/TeamModelValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using FluentValidation.TestHelper;
+using Tsa.Submissions.Coding.Tests.Data;
+using Tsa.Submissions.Coding.WebApi.Entities;
+using Tsa.Submissions.Coding.WebApi.Validators;
+using Xunit;
+
+namespace Tsa.Submissions.Coding.Tests.WebApi.Validators;
+
+[ExcludeFromCodeCoverage]
+public class TeamModelValidatorTests
+{
+    [Theory]
+    [ClassData(typeof(TeamsTestData))]
+    [Trait("TestCategory", "UnitTest")]
+    public void Should_Successfully_Validate_TeamModel(Team team, TeamDataIssues teamDataIssues)
+    {
+        // Arrange
+        var teamModel = team.ToModel();
+
+        var teamModelValidator = new TeamModelValidator();
+
+        // Act
+
+        var testValidationResult = teamModelValidator.TestValidate(teamModel);
+
+        if (teamDataIssues.HasFlag(TeamDataIssues.InvalidParticipants))
+        {
+            testValidationResult.ShouldHaveValidationErrorFor(_ => _.Participants);
+        }
+        else
+        {
+            testValidationResult.ShouldNotHaveValidationErrorFor(_ => _.Participants);
+        }
+
+        if (teamDataIssues.HasFlag(TeamDataIssues.InvalidSchoolNumber))
+        {
+            testValidationResult.ShouldHaveValidationErrorFor(_ => _.SchoolNumber);
+        }
+        else
+        {
+            testValidationResult.ShouldNotHaveValidationErrorFor(_ => _.SchoolNumber);
+        }
+
+        if (teamDataIssues.HasFlag(TeamDataIssues.InvalidTeamNumber))
+        {
+            testValidationResult.ShouldHaveValidationErrorFor(_ => _.TeamNumber);
+        }
+        else
+        {
+            testValidationResult.ShouldNotHaveValidationErrorFor(_ => _.TeamNumber);
+        }
+    }
+}
