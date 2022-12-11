50d0e39b403925905a1774b3875466473997f8f4
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2022-12-11 10:18:21 -0500

    Created Problem entity and model

---
+++ src/Tsa.Submissions.Coding.WebApi/Entities/Problem.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Tsa.Submissions.Coding.WebApi.Entities;
+
+public class Problem : IMongoDbEntity
+{
+    public string? Description { get; set; }
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    [SwaggerSchema(ReadOnly = true)]
+    public string? Id { get; set; }
+
+    public bool IsActive { get; set; }
+}
---
+++ src/Tsa.Submissions.Coding.WebApi/Models/ProblemModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Tsa.Submissions.Coding.WebApi.Models;
+
+public class ProblemModel
+{
+    public string? Description { get; set; }
+
+    public string? Id { get; set; }
+
+    public bool IsActive { get; set; }
+}
