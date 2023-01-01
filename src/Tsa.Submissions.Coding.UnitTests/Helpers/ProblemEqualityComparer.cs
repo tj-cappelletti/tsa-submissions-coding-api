4878819b4d4099ad05f9cc9298d26a7131fad7af
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2023-01-01 15:58:01 -0500

    Created ProblemEqualityComparer

---
+++ src/Tsa.Submissions.Coding.UnitTests/Helpers/ProblemEqualityComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Tsa.Submissions.Coding.WebApi.Entities;
+
+namespace Tsa.Submissions.Coding.UnitTests.Helpers;
+
+internal class ProblemEqualityComparer : IEqualityComparer<Problem>
+{
+    public bool Equals(Problem? x, Problem? y)
+    {
+        return x?.Id == y?.Id;
+    }
+
+    public int GetHashCode(Problem obj)
+    {
+        return obj.GetHashCode();
+    }
+}
