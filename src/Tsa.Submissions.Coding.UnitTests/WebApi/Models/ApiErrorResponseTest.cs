4ea8428d0471a57eba5c08b17d4e2675e1275222
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2023-01-11 10:14:51 -0500

    Added test to improve coverage

---
+++ src/Tsa.Submissions.Coding.UnitTests/WebApi/Models/ApiErrorResponseTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tsa.Submissions.Coding.WebApi.Models;
+using Xunit;
+
+namespace Tsa.Submissions.Coding.UnitTests.WebApi.Models;
+
+[ExcludeFromCodeCoverage]
+public class ApiErrorResponseTest
+{
+    [Fact]
+    [Trait("TestCategory", "UnitTest")]
+    public void Unauthorized_Should_Return_ApiErrorResponse_For_Unauthorized()
+    {
+        // Arrange
+        // Act
+        var apiErrorResponse = ApiErrorResponse.Unauthorized;
+
+        // Assert
+        Assert.Equal((int)ErrorCodes.Unauthorized, apiErrorResponse.ErrorCode);
+        Assert.Equal("Client is unauthorized", apiErrorResponse.Message);
+    }
+}
