b14d07cabe24ad7638cb3d3a2e346ac69e366b68
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2023-02-21 07:59:26 -0500

    Added SubmissionsService to health checks
    Updated associated unit tests
    Improved ServiceStatusModel to dynamicly check IsAlive properties

--- src/Tsa.Submissions.Coding.WebApi/Models/ServicesStatusModel.cs
+++ src/Tsa.Submissions.Coding.WebApi/Models/ServicesStatusModel.cs
@@ -1,12 +1,25 @@
-﻿namespace Tsa.Submissions.Coding.WebApi.Models;
+﻿using System.Linq;
+
+namespace Tsa.Submissions.Coding.WebApi.Models;
 
 public class ServicesStatusModel
 {
-    public bool IsHealthy => ProblemsServiceIsAlive && TeamsServiceIsAlive && TestSetsServiceIsAlive;
+    public bool IsHealthy => EvaluateIsHealthy();
 
     public bool ProblemsServiceIsAlive { get; set; }
+
+    public bool SubmissionsServiceIsAlive { get; set; }
 
     public bool TeamsServiceIsAlive { get; set; }
 
     public bool TestSetsServiceIsAlive { get; set; }
+
+    private bool EvaluateIsHealthy()
+    {
+        var propertyInfos = GetType().GetProperties().Where(_ => _.CanWrite && _.PropertyType == typeof(bool));
+
+        var isAliveList = propertyInfos.Select(propertyInfo => (bool)propertyInfo.GetValue(this)!).ToList();
+
+        return isAliveList.All(isAlive => isAlive);
+    }
 }
