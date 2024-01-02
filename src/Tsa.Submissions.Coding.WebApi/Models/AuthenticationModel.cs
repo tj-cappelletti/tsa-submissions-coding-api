ace91bef673fd4a7fb95aed8856a0a59ec631d9a
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2024-01-02 17:00:27 -0500

    Add AuthenticationModel class

---
+++ src/Tsa.Submissions.Coding.WebApi/Models/AuthenticationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Tsa.Submissions.Coding.WebApi.Models;
+
+public class AuthenticationModel
+{
+    public string? ApiKey { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? UserName { get; set; }
+}
