d5e3472a77546a5dc7ed978634d36697fdbe0fc1
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2024-01-02 16:57:54 -0500

    Added key for Redis connection string

--- src/Tsa.Submissions.Coding.WebApi/Configuration/ConfigurationKeys.cs
+++ src/Tsa.Submissions.Coding.WebApi/Configuration/ConfigurationKeys.cs
@@ -3,5 +3,6 @@
 public static class ConfigurationKeys
 {
     public const string MongoDbConnectionString = "MongoDb";
+    public const string RedisConnectionString = "Redis";
     public const string SubmissionsDatabaseSection = "SubmissionsDatabase";
 }
