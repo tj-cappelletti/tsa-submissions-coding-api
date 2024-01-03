2b980ddd8f4021a440b64b9a418ea1845ed9d628
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2024-01-03 12:24:04 -0500

    Add CacheService and ICacheService interfaces

---
+++ src/Tsa.Submissions.Coding.WebApi/Services/CacheService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Tsa.Submissions.Coding.WebApi.Services;
+
+public class CacheService : ICacheService
+{
+    private readonly IDistributedCache _distributedCache;
+
+    public CacheService(IDistributedCache distributedCache)
+    {
+        _distributedCache = distributedCache;
+    }
+
+    public async Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default)
+    {
+        var value = await _distributedCache.GetStringAsync(key, cancellationToken);
+
+        return value is null ? default : JsonConvert.DeserializeObject<T>(value);
+    }
+
+    public async Task RemoveAsync(string key, CancellationToken cancellationToken = default)
+    {
+        var value = await _distributedCache.GetStringAsync(key, cancellationToken);
+
+        if (value is null) return;
+
+        await _distributedCache.RemoveAsync(key, cancellationToken);
+    }
+
+    public async Task SetAsync<T>(string key, T value, TimeSpan? absoluteExpirationRelativeToNow = null, CancellationToken cancellationToken = default)
+    {
+        var options = new DistributedCacheEntryOptions();
+
+        if (absoluteExpirationRelativeToNow.HasValue)
+        {
+            options.AbsoluteExpirationRelativeToNow = absoluteExpirationRelativeToNow.Value;
+        }
+
+        await _distributedCache.SetStringAsync(key, JsonConvert.SerializeObject(value), options, cancellationToken);
+    }
+}
