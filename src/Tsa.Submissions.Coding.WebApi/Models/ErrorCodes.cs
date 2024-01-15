c9bb59f91af1ad9900357a11d4f4220e1eb2ba7d
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2024-01-15 13:05:51 -0500

    Added error for entities not found
    Added additional properties help explain errors

--- src/Tsa.Submissions.Coding.WebApi/Models/ErrorCodes.cs
+++ src/Tsa.Submissions.Coding.WebApi/Models/ErrorCodes.cs
@@ -5,5 +5,6 @@ public enum ErrorCodes
     Unauthorized = 1000,
     RequiredEntityNotFound = 2000,
     UnexpectedNullValue = 3000,
-    UnexpectedMissingResource = 4000
+    UnexpectedMissingResource = 4000,
+    EntityNotFound = 5000,
 }
