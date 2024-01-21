5b13abc9e7119c2aae6b2ede5fc2bc6c5f9f61bf
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2024-01-21 10:14:36 -0500

    Added model validator to validate UserModel

---
+++ src/Tsa.Submissions.Coding.WebApi/Validators/UserModelValidator.cs
@@ -0,0 +1,97 @@
+﻿using System.Linq;
+using FluentValidation;
+using Tsa.Submissions.Coding.WebApi.Authorization;
+using Tsa.Submissions.Coding.WebApi.Models;
+
+namespace Tsa.Submissions.Coding.WebApi.Validators;
+
+public class UserModelValidator : AbstractValidator<UserModel>
+{
+    public UserModelValidator()
+    {
+        RuleFor(user => user.Password)
+            .Custom((password, context) =>
+            {
+                if (password == null) return;
+
+                if (password.Length < 8)
+                {
+                    context.AddFailure("The password must be at least 8 characters long.");
+                }
+
+                if (!password.Any(char.IsUpper))
+                {
+                    context.AddFailure("The password must contain at least one uppercase letter.");
+                }
+
+                if (!password.Any(char.IsLower))
+                {
+                    context.AddFailure("The password must contain at least one lowercase letter.");
+                }
+
+                if (!password.Any(char.IsDigit))
+                {
+                    context.AddFailure("The password must contain at least one digit.");
+                }
+
+                if (!password.Any(IsSpecialCharacter))
+                {
+                    context.AddFailure("The password must contain at least one special character.");
+                }
+            });
+
+        RuleFor(user => user.Role)
+            .NotEmpty()
+            .IsEnumName(typeof(SubmissionRoles))
+            .WithMessage("A user must have a valid role.");
+
+        RuleFor(user => user.Team)
+            .NotNull()
+            .WithMessage("A user must be associated with a team.");
+
+        RuleFor(user => user.UserName)
+            .NotEmpty()
+            .WithMessage("A user must have a username.");
+    }
+
+    private static bool IsSpecialCharacter(char character)
+    {
+        return character switch
+        {
+            ' ' => true,
+            '!' => true,
+            '"' => true,
+            '#' => true,
+            '$' => true,
+            '%' => true,
+            '&' => true,
+            '\'' => true,
+            '(' => true,
+            ')' => true,
+            '*' => true,
+            '+' => true,
+            ',' => true,
+            '-' => true,
+            '.' => true,
+            '/' => true,
+            ':' => true,
+            ';' => true,
+            '<' => true,
+            '=' => true,
+            '>' => true,
+            '?' => true,
+            '@' => true,
+            '[' => true,
+            '\\' => true,
+            ']' => true,
+            '^' => true,
+            '_' => true,
+            '`' => true,
+            '{' => true,
+            '|' => true,
+            '}' => true,
+            '~' => true,
+            _ => false
+        };
+    }
+}
