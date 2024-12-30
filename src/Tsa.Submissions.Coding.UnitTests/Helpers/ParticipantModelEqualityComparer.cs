9e7cc096325554d2ceab30a9398e61f6476bb717
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2024-12-30 09:12:04 -0500

    Created equality comparers for unit tests

---
+++ src/Tsa.Submissions.Coding.UnitTests/Helpers/ParticipantModelEqualityComparer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Tsa.Submissions.Coding.WebApi.Models;
+
+namespace Tsa.Submissions.Coding.UnitTests.Helpers;
+
+internal class ParticipantModelEqualityComparer : IEqualityComparer<ParticipantModel?>, IEqualityComparer<IList<ParticipantModel>?>
+{
+    public bool Equals(ParticipantModel? x, ParticipantModel? y)
+    {
+        if (ReferenceEquals(x, y)) return true;
+        if (x is null) return false;
+        if (y is null) return false;
+
+        var participantNumbersMatch = x.ParticipantNumber == y.ParticipantNumber;
+        var schoolNumbersMatch = x.SchoolNumber == y.SchoolNumber;
+
+        return participantNumbersMatch &&
+               schoolNumbersMatch;
+    }
+
+    public bool Equals(IList<ParticipantModel>? x, IList<ParticipantModel>? y)
+    {
+        if (ReferenceEquals(x, y)) return true;
+        if (x is null) return false;
+        if (y is null) return false;
+        if (x.Count != y.Count) return false;
+
+        foreach (var leftParticipant in x)
+        {
+            var rightParticipant = y.SingleOrDefault(participant => participant.ParticipantId == leftParticipant.ParticipantId);
+
+            if (!Equals(leftParticipant, rightParticipant)) return false;
+        }
+
+        return true;
+    }
+
+    public int GetHashCode(ParticipantModel? obj)
+    {
+        return obj == null ? 0 : obj.GetHashCode();
+    }
+
+    public int GetHashCode(IList<ParticipantModel>? obj)
+    {
+        return obj == null ? 0 : obj.GetHashCode();
+    }
+}
---
+++ src/Tsa.Submissions.Coding.UnitTests/Helpers/TeamModelEqualityComparer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Tsa.Submissions.Coding.WebApi.Models;
+
+namespace Tsa.Submissions.Coding.UnitTests.Helpers;
+
+[ExcludeFromCodeCoverage]
+internal class TeamModelEqualityComparer : IEqualityComparer<TeamModel?>, IEqualityComparer<IList<TeamModel>?>
+{
+    private readonly ParticipantModelEqualityComparer _participantModelEqualityComparer = new();
+
+    public bool Equals(TeamModel? x, TeamModel? y)
+    {
+        if (ReferenceEquals(x, y)) return true;
+        if (x is null) return false;
+        if (y is null) return false;
+
+        var competitionLevelsMatch = x.CompetitionLevel == y.CompetitionLevel;
+        var idsMatch = x.Id == y.Id;
+        var participantsMatch = _participantModelEqualityComparer.Equals(x.Participants, y.Participants);
+        var schoolNumbersMatch = x.SchoolNumber == y.SchoolNumber;
+        var teamNumbersMatch = x.TeamNumber == y.TeamNumber;
+
+        return competitionLevelsMatch &&
+               idsMatch &&
+               participantsMatch &&
+               schoolNumbersMatch &&
+               teamNumbersMatch;
+    }
+
+    public bool Equals(IList<TeamModel>? x, IList<TeamModel>? y)
+    {
+        if (ReferenceEquals(x, y)) return true;
+        if (x is null) return false;
+        if (y is null) return false;
+        if (x.Count != y.Count) return false;
+
+        foreach (var leftTeamModel in x)
+        {
+            var rightTeamModel = y.SingleOrDefault(teamModel => teamModel.Id == leftTeamModel.Id);
+
+            if (!Equals(leftTeamModel, rightTeamModel)) return false;
+        }
+
+        return true;
+    }
+
+    public int GetHashCode(TeamModel? obj)
+    {
+        return obj == null ? 0 : obj.GetHashCode();
+    }
+
+    public int GetHashCode(IList<TeamModel>? obj)
+    {
+        return obj == null ? 0 : obj.GetHashCode();
+    }
+}
