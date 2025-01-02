415dca826bd7bbaad516a6aef23153839dc082c0
TJ Cappelletti <tcappelletti@microsoft.com>
2025-01-02 11:41:52 -0500

    Ran dotnet format

--- src/Tsa.Submissions.Coding.UnitTests/WebApi/Models/ModelExtensionsTests.cs
+++ src/Tsa.Submissions.Coding.UnitTests/WebApi/Models/ModelExtensionsTests.cs
@@ -190,7 +190,7 @@ public class ModelExtensions
                 }
             ],
             SchoolNumber = teamModel.SchoolNumber,
-            TeamNumber =teamModel.TeamNumber
+            TeamNumber = teamModel.TeamNumber
         };
 
         // Act
