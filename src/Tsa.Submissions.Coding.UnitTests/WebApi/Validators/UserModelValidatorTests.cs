cb4d3a2d0de296ea36293e4b4b71cbb9c4e03900
TJ Cappelletti <tcappelletti@microsoft.com>
2025-01-02 11:33:17 -0500

    Added unit test for UserModelValidator

---
+++ src/Tsa.Submissions.Coding.UnitTests/WebApi/Validators/UserModelValidatorTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using FluentValidation.TestHelper;
+using Tsa.Submissions.Coding.UnitTests.Data;
+using Tsa.Submissions.Coding.WebApi.Models;
+using Tsa.Submissions.Coding.WebApi.Validators;
+using Xunit;
+
+namespace Tsa.Submissions.Coding.UnitTests.WebApi.Validators;
+
+[ExcludeFromCodeCoverage]
+public class UserModelValidatorTests
+{
+    [Theory]
+    [ClassData(typeof(UserModelsTestData))]
+    [Trait("TestCategory", "UnitTest")]
+    public void Should_Successfully_Validate_ProblemModel(UserModel userModel, UserModelDataIssues userModelDataIssues)
+    {
+        // Arrange
+        var userModelValidator = new UserModelValidator();
+
+        // Act
+        var testValidationResult = userModelValidator.TestValidate(userModel);
+
+        // Assert
+        if (userModelDataIssues == UserModelDataIssues.PasswordLength ||
+            userModelDataIssues == UserModelDataIssues.PasswordMissingLowercase ||
+            userModelDataIssues == UserModelDataIssues.PasswordMissingNumber ||
+            userModelDataIssues == UserModelDataIssues.PasswordMissingSpecialCharacter ||
+            userModelDataIssues == UserModelDataIssues.PasswordMissingUppercase)
+        {
+            testValidationResult.ShouldHaveValidationErrorFor(user => user.Password);
+        }
+        else
+        {
+            testValidationResult.ShouldNotHaveValidationErrorFor(user => user.Password);
+        }
+
+        if (userModelDataIssues == UserModelDataIssues.RoleNotValid)
+        {
+            testValidationResult.ShouldHaveValidationErrorFor(user => user.Role);
+        }
+        else
+        {
+            testValidationResult.ShouldNotHaveValidationErrorFor(user => user.Role);
+        }
+
+        if (userModelDataIssues == UserModelDataIssues.ParticipantMissingTeam)
+        {
+            testValidationResult.ShouldHaveValidationErrorFor(user => user.Team);
+        }
+        else
+        {
+            testValidationResult.ShouldNotHaveValidationErrorFor(user => user.Team);
+        }
+    }
+}
