94d6a6ce2e1c3052fc800d1f1c79f71251b54470
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-03-18 09:12:27 -0400

    Added additional helpers and improved method names

--- src/Tsa.Submissions.Coding.UnitTests/Helpers/MockHelpers.cs
+++ src/Tsa.Submissions.Coding.UnitTests/Helpers/MockHelpers.cs
@@ -1,25 +1,44 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.Extensions.Options;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using Moq;
+using Tsa.Submissions.Coding.UnitTests.ExtensionMethods;
 using Tsa.Submissions.Coding.WebApi.Configuration;
-using Tsa.Submissions.Coding.WebApi.Entities;
 
 namespace Tsa.Submissions.Coding.UnitTests.Helpers;
 
 internal static class MockHelpers
 {
-    public static (Mock<IMongoCollection<Submission>>, Mock<IMongoClient>) SetupMockedMongoObjects(string databaseName, string collectionName)
+    public static Mock<IAsyncCursor<T>> CreateMockedAsyncCursor<T>(IEnumerable<T> documents)
+    {
+        var mockedAsyncCursor = new Mock<IAsyncCursor<T>>();
+
+        mockedAsyncCursor
+            .SetupSequence(cursor => cursor.MoveNext(It.IsAny<CancellationToken>()))
+            .Returns(true)
+            .Returns(false);
+
+        mockedAsyncCursor
+            .Setup(cursor => cursor.Current)
+            .Returns(documents);
+
+        return mockedAsyncCursor;
+    }
+
+    public static (Mock<IMongoCollection<T>>, Mock<IMongoClient>) CreateMockedMongoObjects<T>(string databaseName, string collectionName)
     {
         var mockedMongoDatabase = new Mock<IMongoDatabase>();
 
-        var mockedMongoCollection = new Mock<IMongoCollection<Submission>>();
+        var mockedMongoCollection = new Mock<IMongoCollection<T>>();
         mockedMongoCollection
             .Setup(mongoCollection => mongoCollection.Database)
             .Returns(mockedMongoDatabase.Object);
 
         mockedMongoDatabase
-            .Setup(mongoDatabase => mongoDatabase.GetCollection<Submission>(It.Is<string>(name => name == collectionName), null))
+            .Setup(mongoDatabase => mongoDatabase.GetCollection<T>(It.Is<string>(name => name == collectionName), null))
             .Returns(mockedMongoCollection.Object);
 
         var mockedMongoClient = new Mock<IMongoClient>();
@@ -30,7 +49,7 @@ internal static class MockHelpers
         return (mockedMongoCollection, mockedMongoClient);
     }
 
-    public static Mock<IOptions<SubmissionsDatabase>> SetupMockedSubmissionsDatabaseOptions(string databaseName)
+    public static Mock<IOptions<SubmissionsDatabase>> CreateMockedSubmissionsDatabaseOptions(string databaseName)
     {
         var mockedSubmissionsDatabaseOptions = new Mock<IOptions<SubmissionsDatabase>>();
         mockedSubmissionsDatabaseOptions
@@ -41,6 +60,50 @@ internal static class MockHelpers
             });
 
         return mockedSubmissionsDatabaseOptions;
+    }
+
+    public static void SetupMockedMongoCollectionFindAsync<T>(Mock<IMongoCollection<T>> mockedMongoCollection, string filterDefinitionJson,
+        Mock<IAsyncCursor<T>> mockedAsyncCursor)
+    {
+        mockedMongoCollection
+            .Setup(mongoCollection => mongoCollection.FindAsync(
+                It.Is<FilterDefinition<T>>(filter => filter.RenderToJson().Equals(filterDefinitionJson)),
+                It.IsAny<FindOptions<T, T>>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(mockedAsyncCursor.Object)
+            .Verifiable();
+    }
+
+    public static void SetupMockedMongoCollectionFindAsync<T>(Mock<IMongoCollection<T>> mockedMongoCollection, Mock<IAsyncCursor<T>> mockedAsyncCursor)
+    {
+        mockedMongoCollection
+            .Setup(mongoCollection => mongoCollection.FindAsync(
+                Builders<T>.Filter.Empty,
+                It.IsAny<FindOptions<T, T>>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(mockedAsyncCursor.Object)
+            .Verifiable();
+    }
+
+    public static void SetupMockedMongoCollectionRunCommandAsync<T>(Mock<IMongoCollection<T>> mockedMongoCollection,
+        Command<BsonDocument> expectedPingCommand)
+    {
+        mockedMongoCollection
+            .Setup(mongoCollection =>
+                mongoCollection.Database.RunCommandAsync(It.Is<Command<BsonDocument>>(command => ValidatePingCommand(expectedPingCommand, command)),
+                    It.IsAny<ReadPreference>(), It.IsAny<CancellationToken>()))
+            .Verifiable(Times.Once);
+    }
+
+    public static void SetupMockedMongoCollectionRunCommandAsyncThrowsException<T>(Mock<IMongoCollection<T>> mockedMongoCollection,
+        Command<BsonDocument> expectedPingCommand)
+    {
+        mockedMongoCollection
+            .Setup(mongoCollection =>
+                mongoCollection.Database.RunCommandAsync(It.Is<Command<BsonDocument>>(command => ValidatePingCommand(expectedPingCommand, command)),
+                    It.IsAny<ReadPreference>(), It.IsAny<CancellationToken>()))
+            .Throws<Exception>()
+            .Verifiable(Times.Once);
     }
 
     public static bool ValidatePingCommand(Command<BsonDocument> expectedCommand, Command<BsonDocument> actualCommand)
