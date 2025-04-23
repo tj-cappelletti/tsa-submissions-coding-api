45421242697da2a39a56bf85862b37623d78940a
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-04-23 08:13:41 -0400

    Added new key for getting the system API token

--- src/Tsa.Submissions.Coding.WebApi/Configuration/ConfigurationKeys.cs
+++ src/Tsa.Submissions.Coding.WebApi/Configuration/ConfigurationKeys.cs
@@ -5,4 +5,5 @@ public static class ConfigurationKeys
     public const string MongoDbConnectionString = "MongoDb";
     public const string RedisConnectionString = "Redis";
     public const string SubmissionsDatabaseSection = "SubmissionsDatabase";
+    public const string SystemApiToken = "Authentication:SystemApiToken";
 }
